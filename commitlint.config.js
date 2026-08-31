81d206895ba43488a0cf14c3ac5bdee6e9386a9d
truss44 <truss44@wgu.edu>
2026-08-30 20:29:19 -0400

    chore(deps): bump several updates

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'header-max-length': [2, 'always', 150],
     'body-max-line-length': [2, 'always', 200],
   },
 };
